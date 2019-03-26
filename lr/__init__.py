988f581d16a04f31656142fb3b8f8fd225cdce4a
Neil <ws.niu@outlook.com>
2019-03-26 16:46:20 +0800

    整理

--- lr/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Created by Neil on 17-4-12.
-
-
--- lr/sklearn_test.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Created by Neil on 2019/1/7.
-
-
-import numpy as np
-from sklearn.model_selection import train_test_split
-
