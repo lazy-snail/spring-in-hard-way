a491e71542ab8c08b3a313592a68563b6b5bac6f
Neil <ws.niu@outlook.com>
2019-03-27 18:56:34 +0800

    测试demo数据

---
+++ cloudsim/scSim/src/main/java/scheduling/Flag.java
@@ -0,0 +1,16 @@
+package scheduling;
+
+public class Flag {
+	int flag;
+	String name;
+	
+	Flag(){
+		
+	}
+	
+	Flag(int i,String s){
+		flag=i;
+		name=s;
+	}
+
+}
---
+++ cloudsim/scSim/src/main/java/vicc/VmAllocationPolicyFactory.java
@@ -0,0 +1,25 @@
+package vicc;
+
+import org.cloudbus.cloudsim.VmAllocationPolicy;
+import org.cloudbus.cloudsim.power.PowerHost;
+
+import java.util.List;
+
+/**
+ * @author Fabien Hermenier
+ */
+public class VmAllocationPolicyFactory {
+
+    VmAllocationPolicy make(String id, List<PowerHost> hosts) {
+        switch (id) {
+            case "naive":  return new NaiveVmAllocationPolicy(hosts);
+            case "antiAffinity" : return new AntiAffinityVmAllocationPolicy(hosts);
+            case "balance" : return new BalanceVmAllocationPolicy(hosts);
+            case "noViolations" : return new NoViolationsVmAllocationPolicy(hosts);
+            case "statEnergy" : return new StaticEnergyVmAllocationPolicy(hosts);
+            case "dynEnergy" : return new DynamicEnergyVmAllocationPolicy(hosts);
+            case "greedy" : return new GreedyVmAllocationPolicy(hosts);
+        }
+        throw new IllegalArgumentException("No such policy '" + id + "'");
+    }
+}
